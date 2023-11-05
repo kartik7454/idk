e3964b528069eccf7bbd98ac9e27f5af60a23ccb
kartik7454 <kb848067@gmail.com>
2023-11-05 19:06:24 +0530

    my turn

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import { useEffect,useState } from "react"
+
+//components 
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
+
+const Home=()=>{
+
+const[Workouts,setWorkouts]= useState(null)
+
+useEffect( ()=>{
+const fetchWorkouts  = async ()=>{
+const response = await fetch ('/api/workouts/')
+const json = await response.json()
+
+if(response.ok){
+setWorkouts(json)
+}
+}
+
+fetchWorkouts()
+},[])
+
+
+    return(
+        <div className="home">
+        <div className ="workouts">
+            {Workouts && Workouts.map ((workout)=>{
+                return(<WorkoutDetails 
+                    key ={workout._id}
+                    workout={workout}
+                />)
+                    
+
+            })}
+        </div>
+        <WorkoutForm/>
+         </div>
+    )
+}
+
+export default Home
